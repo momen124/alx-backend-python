7ab24f5d9bd2affc51ff75cd512c9d9e348c0fc6
momen124 <momenhossny2004@gmail.com>
2025-05-17 23:26:15 +0300

    Remove unused files

--- 0x00-python_variable_annotations/1-concat.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-""" Basic annotations concat """
-
-
-def concat(str1: str, str2: str) -> str:
-    """ Returns sum of two strings """
-    return str1 + str2
--- 0x00-python_variable_annotations/2-floor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" Basic annotations concat """
-
-import math
-
-
-def floor(n: float) -> int:
-    """ Returns the floor of the float """
-    return math.floor(n)
