950e59ab1ec7f5d00c1feb8b40fa2d8593589659
momen124 <momenhossny2004@gmail.com>
2025-05-30 20:45:54 +0300

    Enhance Conversation and Message viewsets with proper queryset filtering and response handling

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,23 +1,38 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status, filters
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['participants__username']
+    ordering_fields = ['created_at', 'updated_at']
 
-    def perform_create(self, serializer):
-        # Add the requesting user to participants
-        conversation = serializer.save()
-        conversation.participants.add(self.request.user)
+    def get_queryset(self):
+        # Only return conversations where the authenticated user is a participant
+        return Conversation.objects.filter(participants=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['message_body']
+    ordering_fields = ['sent_at']
+
+    def get_queryset(self):
+        # Only return messages from conversations the user is part of
+        return Message.objects.filter(conversation__participants=self.request.user)
 
     def perform_create(self, serializer):
-        # Set the sender to the requesting user
+        # Set the sender to the authenticated user
         serializer.save(sender=self.request.user)
