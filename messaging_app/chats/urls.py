024b338c557e876f80be73fa47653cf5f11c9d92
momen124 <momenhossny2004@gmail.com>
2025-06-04 22:29:42 +0300

    Refactor project structure: remove old files and add new ASGI, WSGI, settings, and URL configurations for the messaging app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ConversationViewSet, MessageViewSet
+from .Views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
