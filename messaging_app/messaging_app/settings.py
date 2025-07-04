8587bb78f6b5146482c223b9f8568d183c89e6ce
momen124 <momenhossny2004@gmail.com>
2025-07-05 02:37:23 +0300

    Add CI/CD pipeline configuration and update dependencies for messaging app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DB'),
-        'USER': os.getenv('MYSQL_USER'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': os.getenv('MYSQL_HOST', 'db'),
+        'NAME': os.getenv('MYSQL_DB', 'messaging_db'),
+        'USER': os.getenv('MYSQL_USER', 'messenger'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'strongpassword'),
+        'HOST': os.getenv('MYSQL_HOST', 'mysql'),  # default to 'mysql'
         'PORT': os.getenv('MYSQL_PORT', '3306'),
     }
 }
